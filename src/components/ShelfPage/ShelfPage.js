df3e73b5cc2f9c68f6ef28735490abf3c9f0bc40
terryokeefe <terryokeefe@users.noreply.github.com>
2021-06-23 15:37:20 -0500

    anything

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -4,7 +4,8 @@ import {useDispatch} from 'react-redux';
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
@@ -14,6 +15,10 @@ function ShelfPage() {
     dispatch({type: 'FETCH_ITEMS'})
   }, []);
   
+const handleDelete = (id) => {
+  console.log('Item to delete:', id)
+  dispatch({type: 'DELETE_ITEM', payload: id})
+}
   return (
     <div className="container">
       <h2>Shelf</h2>
@@ -23,10 +28,13 @@ function ShelfPage() {
           <Grid item sm={3}>
               <Paper>
                   <Card key={image.id}>
-                      <CardActionArea>
+                      <CardContent>
                       <img src={image.image_url} />
                       <Typography gutterBottom variant="h4" component="h2">{image.description}</Typography>
-                      </CardActionArea>
+                      </CardContent>
+                      <CardActions>
+                        <button onClick={() => handleDelete(image.id)}>Delete</button>
+                      </CardActions>
                   </Card>
               </Paper>
           </Grid>
