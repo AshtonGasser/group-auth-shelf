658751f06a609acb90da27b8e7c94471a93e89e7
Ashton Gasser <ashtongasser@gmail.com>
2021-06-23 16:44:29 -0500

    sagas imported

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,16 +2,22 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
+import addItem from './addItem.saga';
+import deleteItem from './deleteitem.saga';
+import fetchItem from './fetchitem.saga';
 //import {addItem, } from "./item.saga"
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
-const addItem = addItem()
+
 // some sagas trigger other sagas, as an example
 // the registration triggers a login
 // and login triggers setting the user
 export default function* rootSaga() {
   yield all([
+    addItem(),
+    deleteItem(),
+    fetchItem(),
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
